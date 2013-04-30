02a35a7c0f91a5428c8ce111429f76e9be321cbe
bholtsclaw <bholtsclaw@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-30 23:00:48 +0000

    1.2.0 release, see changelog
    git-svn-id: https://plugins.svn.wordpress.org/gplus-comments/tags/1.2.0@706205 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/gplus-comments-admin.php
+++ lib/gplus-comments-admin.php
@@ -1,45 +1,34 @@
-<div class="wrap">
 <?php
-if ( !current_user_can( 'manage_options' ) )  {
-    wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
-}
-
-// Processing an option submit form. Set global vars from the form values.
-if( isset($_POST['updateoptions'])  && $_POST['updateoptions']==1) {
-    update_option( 'GPLUSCOMMENTS-WIDTH', $_POST['width'] );
-
-    if ( ! (is_numeric(get_option('GPLUSCOMMENTS-WIDTH')) && get_option('GPLUSCOMMENTS-WIDTH')> 0)) {
-        update_option( 'GPLUSCOMMENTS-WIDTH', 500 );
-    }
-    
-    if( isset($_POST['dynamicwidth'])  && $_POST['dynamicwidth']==1) {
-        update_option( 'GPLUSCOMMENTS-DYNAMICWIDTH', 1 );
-    } else {
-        update_option( 'GPLUSCOMMENTS-DYNAMICWIDTH', 0 );
-    }
-}
+// No direct access
+defined('ABSPATH') or exit;
 ?>
-<form method="POST"  action="?page=gpluscomments">
+<div class="wrap">
+<h2>Google+ Comments for WordPress</h2>
+<form method="POST"  action="options.php">
+<?php settings_fields( 'gplus-comments-options' ); ?>
+<?php $options = get_option( 'gplus-comments' ); ?>
 <table>
     <tr>
-        <td style="text-align:center;" colspan="2"><h2 style="padding: 10px 0; text-decoration:underline;"><span style="color:#0047f1;">G</span><span style="color:#de0c32;">o</span><span style="color:#bc8000;">o</span><span style="color:#0047f1;">g</span><span style="color:#00b018;">l</span><span style="color:#dd172c;">e</span>+ Comments Options</h2></td>
+        <td>Show Facebook Tab:</td>
+        <td><input type="checkbox" name="gplus-comments[show_fb]" value="1" <?php checked( '1', $options['show_fb']);?>></td>
     </tr>
     <tr>
-        <td style="text-align:right;">Width:&nbsp;</td>
-        <td style="padding:10px 5px;"><input type="text" name="width"  <?php if(get_option('GPLUSCOMMENTS-DYNAMICWIDTH') == 1){ echo "readonly"; } ?> value="<?php echo get_option('GOOGLEPLUSCOMMENTS-WIDTH')?>" />
+        <td>Show WordPress Tab:</td>
+        <td><input type="checkbox" name="gplus-comments[show_wp]" value="1" <?php checked( '1', $options['show_wp']);?>></td>
     </tr>
     <tr>
-        <td style="text-align:right;">Dynamic Width:&nbsp;</td>
-        <td style="padding:10px 5px;"><input type="checkbox" name="dynamicwidth" <?php if(get_option('GPLUSCOMMENTS-DYNAMICWIDTH') == 1){echo "checked=\"checked\"";} ?> value="1" /> <i> [Fixes Google+ comment bug for responsive sites]</i></td>
+        <td>Show Disqus Tab:</td>
+        <td><input type="checkbox" name="gplus-comments[show_disqus]" value="1" <?php checked( '1', $options['show_disqus']);?>></td>
     </tr>
     <tr>
-        <td style="text-align:right; padding:20px 0 0 0;">
-            <input type="hidden" name="updateoptions" value="1" />
-            <input type="submit" name="Submit" value="Submit" />
-        </td>
-        <td></td>
+        <td>Disqus Shortname:</td>
+        <td><input type="text" name="gplus-comments[disqus_shortname]" value="<?php echo $options['disqus_shortname'];?>"> ( Required if Showing Disqus Tab )</td>
+    </tr>
+    <tr>
+        <td>Show Trackbacks Tab:</td>
+        <td><input type="checkbox" name="gplus-comments[show_trackbacks]" value="1" <?php checked( '1', $options['show_trackbacks']);?>></td>
     </tr>
 </table>
-
+    <?php submit_button(); ?>
 </form>
 </div>
