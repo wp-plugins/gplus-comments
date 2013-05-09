1494b69e0eab5ac193a0406e8dd1585ea66650cd
bholtsclaw <bholtsclaw@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-09 01:47:16 +0000

    release 1.4.3
    git-svn-id: https://plugins.svn.wordpress.org/gplus-comments/tags/1.4.3@710149 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/comments-container.php
+++ templates/comments-container.php
@@ -108,42 +108,12 @@ if(empty($options['tab_order'])) {
 
   <?php if(in_array('wordpress', $tab_order)) : ?>
   <div id="wordpress-tab" class="block clearfix content-tab">
-
-<?php if (have_comments()) : ?>
-    <h6 id="comments">
-			<?php
-				printf( _n('One comment on &ldquo;%2$s&rdquo;', '%1$s comments on &ldquo;%2$s&rdquo;', get_comments_number()),
-					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>');
-			?>
-    </h6>
-
-    <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
-    <div class="navigation">
-        <div class="previous"><?php previous_comments_link(__( '&#8249; Older comments','responsive' )); ?></div><!-- end of .previous -->
-        <div class="next"><?php next_comments_link(__( 'Newer comments &#8250;','responsive', 0 )); ?></div><!-- end of .next -->
-    </div><!-- end of.navigation -->
-    <?php endif; ?>
-
-    <ol class="commentlist">
-        <?php wp_list_comments('avatar_size=60&type=comment'); ?>
-    </ol>
-
-    <?php else :
-      echo "<p class='nocomments'>No Comments.</p>";
-    endif;
-    if (comments_open()) :
-    $fields = array(
-        'author' => '<p class="comment-form-author">' . '<label for="author">' . __('Name','responsive') . '</label> ' . ( $req ? '<span class="required">*</span>' : '' ) .
-        '<input id="author" name="author" type="text" value="' . esc_attr($commenter['comment_author']) . '" size="30" /></p>',
-        'email' => '<p class="comment-form-email"><label for="email">' . __('E-mail','responsive') . '</label> ' . ( $req ? '<span class="required">*</span>' : '' ) .
-        '<input id="email" name="email" type="text" value="' . esc_attr($commenter['comment_author_email']) . '" size="30" /></p>',
-        'url' => '<p class="comment-form-url"><label for="url">' . __('Website','responsive') . '</label>' .
-        '<input id="url" name="url" type="text" value="' . esc_attr($commenter['comment_author_url']) . '" size="30" /></p>',
-    );
-    $defaults = array('fields' => apply_filters('comment_form_default_fields', $fields));
-    comment_form($defaults);
-    ?>
-    <?php endif; ?>
+  <?php
+  if (file_exists(TEMPLATEPATH . '/comments.php'))
+  {
+    include_once TEMPLATEPATH . '/comments.php';
+  }
+  ?>
   </div> <!--//wp-tab -->
   <?php endif; ?>
 
