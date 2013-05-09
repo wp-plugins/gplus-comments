4b1b78d5fa2b56dc1c816298ccac6c4788de52e4
bholtsclaw <bholtsclaw@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-09 01:47:16 +0000

    release 1.4.3
    git-svn-id: https://plugins.svn.wordpress.org/gplus-comments/trunk@710149 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gplus-comments.php
+++ gplus-comments.php
@@ -8,7 +8,7 @@ Author URI: http://www.brandonholtsclaw.com/
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 Donate link: http://www.wepay.com/donations/brandonholtsclaw
-Version: 1.4.2
+Version: 1.4.3
 */
 
 /* * *     !! WORDPRESS DEVELOPERS AND THEMERS : PLEASE READ BEFORE YOU EDIT THIS FILE.
@@ -35,7 +35,7 @@ if (version_compare(phpversion(), '5.3', '<'))
 
 function gplus_comments_init()
 {
-  define( 'GPLUS_COMMENTS_VERSION', '1.4.2' );
+  define( 'GPLUS_COMMENTS_VERSION', '1.4.3' );
   define( 'GPLUS_COMMENTS_DEFAULT_TAB_ORDER', 'gplus,facebook,wordpress' );
   defined('GPLUS_COMMENTS_DEBUG') or define('GPLUS_COMMENTS_DEBUG', false);
   defined('GPLUS_COMMENTS_DIR') or define('GPLUS_COMMENTS_DIR', dirname(__FILE__));
