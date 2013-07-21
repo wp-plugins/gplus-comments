0d288f8625e29136e1640b7f4cc3c17a9b62a6c5
bholtsclaw <bholtsclaw@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-21 22:21:50 +0000

    1.5.2 updates
    git-svn-id: https://plugins.svn.wordpress.org/gplus-comments/tags/1.5.2@744132 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/templates/partials/facebook.php
+++ includes/templates/partials/facebook.php
@@ -7,19 +7,15 @@
 defined('ABSPATH') or exit;
 
 ?>
-<!-- fb-tab -->
-<div id="facebook-tab" class="clearfix">
-  <div id="fb-root"></div>
-  <div id="fb-comments">Loading Facebook Comments ...</div>
-  <script type="text/javascript">
-    jQuery(document).ready(function($)
-    {
-      $('#fb-comments').html('<div class="fb-comments" data-width="'+window.comment_tab_width+'" data-href="<?php echo the_permalink(); ?>" data-num-posts="20" data-colorscheme="light" data-mobile="auto"></div>');
-    });
-  </script>
-  <script async type="text/javascript" src="//connect.facebook.net/en_US/all.js#xfbml=1">FB.init();</script>
-  <noscript>Please enable JavaScript to view the <a href="https://www.facebook.com/">comments powered by Facebook.</a></noscript>
-</div>
-<!-- //fb-tab -->
+<div id="fb-root"></div>
+<div id="fb-comments">Loading Facebook Comments ...</div>
+<script type="text/javascript">
+  jQuery(document).ready(function($)
+  {
+    $('#fb-comments').html('<div class="fb-comments" data-width="'+window.comment_tab_width+'" data-href="<?php echo the_permalink(); ?>" data-num-posts="20" data-colorscheme="light" data-mobile="auto"></div>');
+  });
+</script>
+<script async type="text/javascript" src="//connect.facebook.net/en_US/all.js#xfbml=1">FB.init();</script>
+<noscript>Please enable JavaScript to view the <a href="https://www.facebook.com/">comments powered by Facebook.</a></noscript>
 
 
