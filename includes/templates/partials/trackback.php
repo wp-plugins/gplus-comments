b29bebbee1b1ca081a4ba42f8f936c6a8adcffb3
bholtsclaw <bholtsclaw@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-21 17:50:07 +0000

    release 1.5.1
    git-svn-id: https://plugins.svn.wordpress.org/gplus-comments/tags/1.5.1@744015 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/templates/partials/trackback.php
+++ includes/templates/partials/trackback.php
@@ -10,7 +10,7 @@ defined('ABSPATH') or exit;
 <!-- tb-tab -->
 <div id="trackback-tab" class="content-tab clearfix">
   <?php if (!empty($comments_by_type['pings'])) : ?>
-    <h6 id='pings'><?php printf( __( '%1$d %2$s for "%3$s"'), count($comments_by_type['pings']), __('Trackbacks'), get_the_title() )?></h6>
+    <!-- <h6 id='pings'><?php printf( __( '%1$d %2$s for "%3$s"'), count($comments_by_type['pings']), __('Trackbacks'), get_the_title() )?></h6> -->
     <ol class="commentlist">
       <?php wp_list_comments('type=pings&max_depth=1'); ?>
     </ol>
