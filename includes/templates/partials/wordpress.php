154e7bac869ca989233586cce5c7a8505586aec8
bholtsclaw <bholtsclaw@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-22 02:38:33 +0000

    1.5.4 release
    git-svn-id: https://plugins.svn.wordpress.org/gplus-comments/tags/1.5.4@744196 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/templates/partials/wordpress.php
+++ includes/templates/partials/wordpress.php
@@ -6,15 +6,8 @@
  */
 defined('ABSPATH') or exit;
 
-?>
-<!-- wp-tab -->
-<div id="wordpress-tab" class="clearfix">
-<?php
 if (file_exists(TEMPLATEPATH . '/comments.php')) {
   include_once TEMPLATEPATH . '/comments.php';
 } elseif (file_exists(TEMPLATEPATH . '/includes/comments.php')) {
   include_once TEMPLATEPATH . '/includes/comments.php';
 }
-?>
-</div>
-<!-- //wp-tab -->
